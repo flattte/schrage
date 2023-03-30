476236d50fd4d6815a9bd5a78f52093afb050004
flattte <jakubwieczorek01@gmail.com>
2023-03-30 16:46:50 +0200

    mvoe macros

--- src/mods/mod.rs
+++ src/mods/mod.rs
@@ -1,4 +1,36 @@
 pub mod task;
 pub mod std_heaps;
 pub mod std_vecs;
-pub mod macro_t;
+
+pub(crate) mod macro_t {
+    #[macro_export]
+    #[allow(unused)]
+    macro_rules! tasks {
+        () => {
+            vec![
+                Task::new(30, 3, 8),
+                Task::new(20, 4, 21),
+                Task::new(10, 5, 7),
+                Task::new(11, 7, 24),
+                Task::new(30, 2, 0),
+                Task::new(13, 6, 26),
+                Task::new(0, 6, 17),
+            ]
+        };
+    }
+    #[macro_export]
+    #[allow(unused)]
+    macro_rules! correct_order {
+        () => {
+            vec![
+                Task::new(0, 6, 17),
+                Task::new(10, 5, 7),
+                Task::new(13, 6, 26),
+                Task::new(11, 7, 24),
+                Task::new(20, 4, 21),
+                Task::new(30, 3, 8),
+                Task::new(30, 2, 0),
+            ]
+        };
+    }
+}
