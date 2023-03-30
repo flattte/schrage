dc22f5a89ff769ac781eb1b32fd0d4a1b793ab89
flattte <jakubwieczorek01@gmail.com>
2023-03-30 20:54:31 +0200

    restructure

--- src/heap/heap_binary.rs
+++ src/heap/heap_binary.rs
@@ -17,19 +17,68 @@ impl<T: Ord> HeapTree<T> {
         self.sift_up(self.heap.len() - 1);
     }
 
-    pub fn pop(&mut self, elem: T) -> Option<T> {
-        if self.heap.is_empty() {
-            return None
+    pub fn pop(&mut self) -> Option<T> {
+        if self.heap.len() == 0 {
+            return None;
         }
-        
-        let elem = self.heap.ta
+
+        if self.heap.len() == 1 {
+            return self.heap.pop();
+        }
+
+        let result = self.heap.swap_remove(0);
+        self.sift_down(0);
+        Some(result)
     }
 
     pub fn sift_down(&mut self, starting_node: usize) {
+        let left_child = 2 * starting_node + 1;
+        let right_child = 2 * starting_node + 2;
+        let mut smallest = starting_node;
 
+        if left_child < self.heap.len() && self.heap[left_child] < self.heap[smallest] {
+            smallest = left_child;
+        }
+
+        if right_child < self.heap.len() && self.heap[right_child] < self.heap[smallest] {
+            smallest = right_child;
+        }
+
+        if smallest != starting_node {
+            self.heap.swap(smallest, starting_node);
+            self.sift_down(smallest);
+        }
     }
 
     pub fn sift_up(&mut self, starting_node: usize) {
+        let mut current = starting_node;
+        let mut parent = (current.saturating_sub(1)) / 2;
+        while current > 0 && self.heap[current] < self.heap[parent] {
+            self.heap.swap(current, parent);
+            current = parent;
+            parent = (current.saturating_sub(1)) / 2;
+        }
+    }
+}
 
+#[cfg(test)]
+mod tests {
+    use crate::mods::task::Task;
+
+    use super::*;
+
+    #[test]
+    fn test_heap() {
+        let mut heap: HeapTree<Task> = HeapTree::new();
+        heap.push(Task::new(1, 1, 1));
+        heap.push(Task::new(2, 2, 2));
+        heap.push(Task::new(3, 3, 3));
+        heap.push(Task::new(4, 4, 4));
+
+        assert_eq!(heap.pop(), Some(Task::new(1, 1, 1)));
+        assert_eq!(heap.pop(), Some(Task::new(2, 2, 2)));
+        assert_eq!(heap.pop(), Some(Task::new(3, 3, 3)));
+        assert_eq!(heap.pop(), Some(Task::new(4, 4, 4)));
+        assert_eq!(heap.pop(), None);
     }
 }
--- src/mods/std_heaps.rs
+++ src/mods/std_heaps.rs
@@ -1,67 +1,9 @@
 #![allow(unused)]
-use crate::mods::task::Task;
+use crate::mods::task::{Task, QInvariant, RInvariant};
 use std::cmp::{max, Ordering};
 use std::collections::BinaryHeap;
 
-#[derive(Eq, Debug)]
-pub struct QInvariant(Task);
 
-impl From<Task> for RInvariant {
-    fn from(task: Task) -> Self {
-        RInvariant(task)
-    }
-}
-impl From<&Task> for RInvariant {
-    fn from(task: &Task) -> Self {
-        RInvariant(*task)
-    }
-}
-impl PartialEq for RInvariant {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.r == other.0.r
-    }
-}
-impl PartialOrd for RInvariant {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(other.0.r.cmp(&self.0.r))
-    }
-}
-// since RInvariant has to be in descending order
-//the order of comparison is (other) <=> (self) and not the other way around
-impl Ord for RInvariant {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.0.r.cmp(&self.0.r)
-    }
-}
-
-#[derive(Eq, Debug)]
-pub struct RInvariant(Task);
-
-impl From<Task> for QInvariant {
-    fn from(task: Task) -> Self {
-        QInvariant(task)
-    }
-}
-impl From<&Task> for QInvariant {
-    fn from(task: &Task) -> Self {
-        QInvariant(*task)
-    }
-}
-impl PartialEq for QInvariant {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.q == other.0.q
-    }
-}
-impl PartialOrd for QInvariant {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.0.q.cmp(&other.0.q))
-    }
-}
-impl Ord for QInvariant {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.0.q.cmp(&other.0.q)
-    }
-}
 
 #[derive(Debug)]
 pub struct ShrageContextBH {
