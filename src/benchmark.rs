9bd530a9543f9f99ec75e2d347398c50119fa011
flattte <jakubwieczorek01@gmail.com>
2023-04-01 23:07:38 +0200

    benchmarking done

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -1,12 +1,17 @@
 #![allow(unused)]
+#[macro_use]
 use core::ops::Range;
+use std::time::Duration;
 use rand::{thread_rng, Rng};
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use lazy_static::lazy_static;
+use criterion::{
+    black_box, criterion_group, criterion_main, BenchmarkId, Criterion, PlotConfiguration,
+    SamplingMode,
+};
 mod mods;
-use crate::mods::task::Task;
+use crate::mods::{heap_binary::*, std_heaps::*, std_vecs::*, task::Task};
 
-
-fn gen_gaussian(
+fn gen_uniform(
     amount: usize,
     r_vals: &Range<u32>,
     p_vals: &Range<u32>,
@@ -23,14 +28,91 @@ fn gen_gaussian(
     tasks
 }
 
+fn gen_tasks() -> Vec<Vec<Task>> {
+    let args: Vec<(usize, Range<u32>, Range<u32>, Range<u32>)> = (1..=50)
+        .into_iter()
+        .step_by(5)
+        .map(|n| {
+            (
+                (1000 * n) as usize,
+                (0..100 * n),
+                (0..1000 * n),
+                (0..1000 * n),
+            )
+        })
+        .collect();
 
-
-fn bench_alg_on_random_time() -> usize {
-    let tasks = gen_gaussian(10000, &(0..10000), &(0..10000), &(0..10000));
-    
-    0
+    args.iter()
+        .map(|arg| gen_uniform(arg.0, &arg.1, &arg.2, &arg.3))
+        .collect()
 }
-fn benches() {}
 
-//criterion_group!(benches, );
-criterion_main!(benches);
+lazy_static!{
+    static ref DATA: Vec<Vec<Task>> = gen_tasks();
+}
+
+
+fn bench_algs_preemptive(c: &mut Criterion) {
+    let sets_of_tasks: &Vec<Vec<Task>> = &*DATA;
+    let mut group = c.benchmark_group("preemptive salgs on random uniform data");
+    group
+        .sample_size(30)
+        .sampling_mode(SamplingMode::Flat)
+        .plot_config(PlotConfiguration::default())
+        .warm_up_time(Duration::from_secs(1));
+
+    for (idx, tasks) in sets_of_tasks.iter().enumerate() {
+        group.bench_with_input(
+            BenchmarkId::new("schrage_preemptive_heaps_std_cmax", &idx),
+            tasks,
+            |bencher, input| bencher.iter(|| schrage_preemptive_heaps_std_cmax(input.clone())),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("schrage_preemptive_custom_heaps_cmax", &idx),
+            tasks,
+            |bencher, input| bencher.iter(|| schrage_preemptive_custom_heaps_cmax(input.clone())),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("schrage_preemptive_vecs_cmax", &idx),
+            tasks,
+            |bencher, input| bencher.iter(|| schrage_preemptive_vecs_cmax(input.clone())),
+        );
+    }
+    group.finish();
+}
+
+fn bench_algs(c: &mut Criterion) {
+    let sets_of_tasks:&Vec<Vec<Task>> = &*DATA;
+    let mut group = c.benchmark_group("Non preemptive algs on random uniform data");
+    group
+        .sampling_mode(SamplingMode::Flat)
+        .plot_config(PlotConfiguration::default())
+        .warm_up_time(Duration::from_secs(1));
+
+    for (idx, tasks) in sets_of_tasks.iter().enumerate() {
+        group.bench_with_input(
+            BenchmarkId::new("schrage_heaps_std_cmax", &idx),
+            tasks,
+            |bencher, input| bencher.iter(|| schrage_heaps_std_cmax(input.clone())),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("schrage_custom_heaps_cmax", &idx),
+            tasks,
+            |bencher, input| bencher.iter(|| schrage_custom_heaps_cmax(input.clone())),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("schrage_vecs_sort_q_cmax", &idx),
+            tasks,
+            |bencher, input| bencher.iter(|| schrage_vecs_sort_q_cmax(input.clone())),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("schrage_vecs_sort_r_cmax", &idx),
+            tasks,
+            |bencher, input| bencher.iter(|| schrage_vecs_sort_r_cmax(input.clone())),
+        );
+    }
+    group.finish();
+}
+
+criterion_group!(bench, bench_algs, bench_algs_preemptive);
+criterion_main!(bench);
