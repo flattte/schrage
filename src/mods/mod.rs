6aca2fe3b1f4b1af1814803753fc5b7cdd08a57d
flattte <jakubwieczorek01@gmail.com>
2023-04-02 14:33:08 +0200

    nearly purely functional gen_tasks

--- src/mods/mod.rs
+++ src/mods/mod.rs
@@ -2,23 +2,23 @@ pub mod std_heaps;
 pub mod std_vecs;
 pub mod heap_binary;
 pub mod heap_tree;
-pub mod task {
-use std::fmt::Display;
 
+pub mod task {
     #[derive(Eq, PartialEq, PartialOrd, Ord, Copy, Clone, Debug)]
     pub struct Task {
         pub r: u32, // ready time
         pub p: u32, // working time
         pub q: u32, // cooldown time
     }
-
+    
     impl Task {
         #[allow(unused)]
         pub fn new(r: u32, p: u32, q: u32) -> Task {
             Task { r, p, q }
         }
     }
-
+    
+    use std::fmt::Display;
     impl Display for Task {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             write!(f, "({}, {}, {})", self.r, self.p, self.q)
@@ -100,35 +100,35 @@ use std::fmt::Display;
     }
 
     #[macro_export]
-#[allow(unused)]
-macro_rules! tasks {
-    () => {
-        vec![
-            Task::new(30, 3, 8),
-            Task::new(20, 4, 21),
-            Task::new(10, 5, 7),
-            Task::new(11, 7, 24),
-            Task::new(30, 2, 0),
-            Task::new(13, 6, 26),
-            Task::new(0, 6, 17),
-        ]
-    };
-}
-#[macro_export]
-#[allow(unused)]
-macro_rules! correct_order {
-    () => {
-        vec![
-            Task::new(0, 6, 17),
-            Task::new(10, 5, 7),
-            Task::new(13, 6, 26),
-            Task::new(11, 7, 24),
-            Task::new(20, 4, 21),
-            Task::new(30, 3, 8),
-            Task::new(30, 2, 0),
-        ]
-    };
-}
+    #[allow(unused)]
+    macro_rules! tasks {
+        () => {
+            vec![
+                Task::new(30, 3, 8),
+                Task::new(20, 4, 21),
+                Task::new(10, 5, 7),
+                Task::new(11, 7, 24),
+                Task::new(30, 2, 0),
+                Task::new(13, 6, 26),
+                Task::new(0, 6, 17),
+            ]
+        };
+    }
+    #[macro_export]
+    #[allow(unused)]
+    macro_rules! correct_order {
+        () => {
+            vec![
+                Task::new(0, 6, 17),
+                Task::new(10, 5, 7),
+                Task::new(13, 6, 26),
+                Task::new(11, 7, 24),
+                Task::new(20, 4, 21),
+                Task::new(30, 3, 8),
+                Task::new(30, 2, 0),
+            ]
+        };
+    }
 }
 
 
